4ca472551c0af6593e059c46d2f399dbddba0efd
Augusto Maza <debianmaza@gmail.com>
2025-01-07 14:38:34 -0300

    commit #3 07-01-2025 se arregla la tabla fc_obras_sociales

--- dump_data_base.sh
+++ dump_data_base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 fecha=`date +%d-%m-%Y`
 archivo="sqls/farmacia_calendar-$fecha.sql"
-#mysqldump --user=root --password=slack142 --host=slackzone.ddns.net gesdoju > $archivo
-mysqldump --user=root --password=slack142 --host=slackzone.ddns.net farmacia_calendar > $archivo
+#mysqldump --user=root --password=slack142 --host=slackzone.ddns.net farmacia_calendar > $archivo
+mysqldump --user=root --password=slack142 --host=localhost farmacia_calendar > $archivo
 chmod 777 $archivo
 
 
