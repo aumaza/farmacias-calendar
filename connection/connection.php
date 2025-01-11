07394565136b6ed7469d5a96095ba1f92a0daa90
Augusto <augusto@bangho-desktop.bangho-desktop>
2025-01-11 09:42:53 -0300

    commit #8 11-01-2025 se modifica archivo connection.php

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,11 @@
 <?php
 
-//$dbhost = 'slackzone.ddns.net';
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = 'slack142';
-$dbase = 'farmacia_calendar';
+//$dbhost = 'slackzone.ddns.net'; // host de desarrollo
+$dbhost = 'localhost'; // host de produccion
+$dbuser = 'root'; // usuario
+// $dbpass = 'slack142'; // password para servidor de desarrollo
+$dbpass = 'proteo601'; // password para el servidor de produccion
+$dbase = 'farmacia_calendar'; // nombre de la base de datos
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbase);
 
 
